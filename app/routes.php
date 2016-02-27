cbc2c22b79b247c0b165a013f755a30173fd82f5
jeremy dolle <jeremy.dolle34@gmail.com>
2016-02-27 19:17:31 +0100

    Travail sur le visuel du site

--- app/routes.php
+++ app/routes.php
@@ -4,3 +4,9 @@
 $app->get('/', function () use ($app) {
     return $app['twig']->render('index.html.twig');
 });
+
+// Category page
+$app->get('/', function () use ($app) {
+    $vinyl = $app['dao.vinyl']->findAll();
+    return $app['twig']->render('category.html.twig', array('vinyl' => $vinyl));
+});
