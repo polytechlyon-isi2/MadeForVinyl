be0351af376908ae297134251315cd734ebc1cdf
jeremy dolle <jeremy.dolle34@gmail.com>
2016-03-31 20:16:26 +0200

    Ajout de la page de chargement et Js amélioré pour meilleur rendu 'fondu'

--- web/js/anim.js
+++ web/js/anim.js
@@ -1,7 +1,22 @@
 $(function(){
-	$("h1").hide();
-	$(".container-content").hide();
 
-	$("h1").fadeIn(1000);
-	$(".container-content").fadeIn(1000);
+	$(".dropdown-menu li").click(function(){
+		$(".container-content").fadeOut(1000);
+		$("h1").hide("slide");
+	});
+
+	$(".navbar-brand").click(function(){
+		$(".container-content").fadeOut(1000);
+		$("h1").hide("slide");
+	});
+
+	$(document).ready(function(){
+		$("h1").hide().show("slide");
+		$(".container-content").hide().fadeToggle(1000);
+	});
+	
+	$(window).load(function() {
+     	$('#loading').hide();
+  	});
+
 });
